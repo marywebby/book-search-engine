bcf30f1c0c72c8e33188ab3a606ef8556dc42159
Mary Webby <MaryWebby@Marys-MacBook-Air-3.local>
2023-04-30 18:31:16 -0400

    installed dependancies and set up app.js in client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+// import apollo client, provider, and memory cache 
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
+
+// importing set client from apollo server 
+import { setContext } from '@apollo/client/link/context';
+
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+// created a new apollo client to work with our db 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// request middleware that jwt token to every request as an authorization header 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+// setting up client to let the authlink work before the requests to graphql come through
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
+    <ApolloProvider client = {client}>
     <Router>
       <>
         <Navbar />
@@ -16,6 +46,7 @@ function App() {
         </Switch>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
