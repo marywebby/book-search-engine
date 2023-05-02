f449a4181eba998f2cef14e69c02e3f688b21841
Mary Webby <MaryWebby@Marys-MacBook-Air-3.local>
2023-05-02 17:24:26 -0400

    herko deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,21 @@
 import React from 'react';
+// import apollo providor, client and in memory cache
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
-// import apollo client, provider, and memory cache 
-import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
-
-// importing set client from apollo server 
-import { setContext } from '@apollo/client/link/context';
-
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
-// created a new apollo client to work with our db 
-const httpLink = createHttpLink({
-  uri: '/graphql',
-});
 
-// request middleware that jwt token to every request as an authorization header 
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
-
-// setting up client to let the authlink work before the requests to graphql come through
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
+  uri: '/graphql', 
+  cache: new InMemoryCache()
 });
+
 
 function App() {
   return (
-    <ApolloProvider client = {client}>
+    <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
